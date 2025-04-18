578f6a8536cedf5f059d2193519ebe09d2455d1c
purvaamidha <krishan.purvaa@gmail.com>
2025-04-18 16:39:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// Isotope for filtering
+// Isotope.js for filtering images by category
 var iso = new Isotope('.gallery', {
   itemSelector: '.gallery-item',
-  layoutMode: 'fitRows'
+  layoutMode: 'fitRows',
 });
 
 document.querySelectorAll('.filters button').forEach(button => {
-  button.addEventListener('click', () => {
-    let filterValue = button.getAttribute('data-filter');
+  button.addEventListener('click', function () {
+    const filterValue = button.getAttribute('data-filter');
     iso.arrange({ filter: filterValue });
+
+    // Add active class to the clicked filter button
+    document.querySelectorAll('.filters button').forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
   });
 });
 
-// GLightbox for fullscreen view
+// GLightbox for fullscreen image preview
 const lightbox = GLightbox({
-  selector: '.glightbox'
+  selector: '.glightbox',  // All images with this class will open in a lightbox
+  touchNavigation: true,    // Enable touch navigation for mobile
+  loop: true,               // Loop through images when in the lightbox
+  zoomable: true,           // Allow zooming of images
+  openEffect: 'fade',       // Add fade effect when opening the lightbox
+  closeEffect: 'fade',      // Add fade effect when closing the lightbox
+  closeButton: true         // Enable close button in lightbox
 });
